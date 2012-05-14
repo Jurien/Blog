144218fa42ccc59b17c45426a23c0c257c3a1e7e
Jurien matheu <jurien.matheu@gmail.com>
2012-05-14 12:04:18 +0200

    ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
+  get "home/index"
+
+  resources :posts
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
