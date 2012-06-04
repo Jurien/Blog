804a7fd0803c1463a101d6b6759c59ff7b16e91b
Jurien matheu <jurien.matheu@gmail.com>
2012-06-04 11:53:08 +0200

    nieuw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+    def set_locale
+        I18n.locale = get_local_from_browser_setting
+    end
+
+    def get_local_from_browser_setting
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
 end
